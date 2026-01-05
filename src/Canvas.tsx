78dc5d7c0e01c6eebd1b54e1d4bcaa1ff07a5b84
Ciro Mirkin <ciromirkin@gmail.com>
2026-01-05 02:59:15 -0300

    mejora los estilos al hacer dragging

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,8 +1,12 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { useFiles } from './useFiles'
 
 export function Canvas() {
     const { uploadedFiles, setUploadedFiles } = useFiles()
+    const [ dragging, setDragging ] = useState({
+        state: false,
+        imageId: '',
+    })
     const canvasRef = useRef<HTMLDivElement>(null)
 
     const handleDrop = (e: React.DragEvent) => {
@@ -19,6 +23,7 @@ export function Canvas() {
             setUploadedFiles(prev =>
                 prev.map(img => (img.id === id ? { ...img, x: nextX, y: nextY, zIndex: 1 } : {...img, zIndex: 0}))
             )
+            setDragging({ state: false, imageId: '' })
         }
     }
 
@@ -42,15 +47,24 @@ export function Canvas() {
                     src={img.src}
                     draggable
                     onDragStart={(e) => {
+                        const target = e.target as HTMLImageElement
+
                         e.dataTransfer.setData('image-id', img.id)
-                        const rect = (e.target as HTMLImageElement).getBoundingClientRect()
-                        e.dataTransfer.setData('offset-x', (e.clientX - rect.left).toString())
-                        e.dataTransfer.setData('offset-y', (e.clientY - rect.top).toString())
+                        setDragging({ state: true, imageId: img.id })
+                       
+                        const rect = (target).getBoundingClientRect()
+                        const offsetX = e.clientX - rect.left
+                        const offsetY = e.clientY - rect.top
+                        e.dataTransfer.setData('offset-x', offsetX.toString())
+                        e.dataTransfer.setData('offset-y', offsetY.toString())
+                        e.dataTransfer.setDragImage(target, offsetX, offsetY)
                     }}
                     style={{
                         position: 'absolute', left: `${img.x}px`, top: `${img.y}px`,
                         width: '200px', cursor: 'grab', borderRadius: '4px', border: '2px solid white',
                         boxShadow: '0 8px 16px rgba(0,0,0,0.15)', zIndex: `${img.zIndex}`,
+                        opacity: `${dragging.state && dragging.imageId === img.id ? 0.25 : 1}`,
+                        
                     }}
                 />
             ))}
