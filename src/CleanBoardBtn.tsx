049cb8f97a7d42778b78119c4d1806dd4dc33112
Ciro Mirkin <ciromirkin@gmail.com>
2026-02-24 00:00:55 -0300

    feat: mejorar boton limpiar tablero

--- src/CleanBoardBtn.tsx
+++ src/CleanBoardBtn.tsx
@@ -1,14 +1,22 @@
 import { useFiles } from './useFiles'
 
 function CleanBoardBtn() {
-    const { setUploadedFiles } = useFiles()
+    const { setUploadedFiles, clearArrows } = useFiles()
 
     const handleClearBoard = () => {
-        setUploadedFiles([])
+        if (confirm('¿Estás seguro de limpiar todo el tablero?')) {
+            setUploadedFiles([])
+            clearArrows()
+        }
     }
 
     return (
-        <button onClick={handleClearBoard}>Limpiar Tablero</button>
+        <button onClick={handleClearBoard} className="toolbar-button" data-tooltip="Limpiar tablero">
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
+                <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
+                <path d="M3 3v5h5" />
+            </svg>
+        </button>
     )
 }
 
