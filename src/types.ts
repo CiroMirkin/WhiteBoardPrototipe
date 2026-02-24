76217b4863ff53883b21c988e7eb34157b8292f0
Ciro Mirkin <ciromirkin@gmail.com>
2026-02-23 23:59:57 -0300

    feat: agregar tipo Arrow al sistema de tipos

--- src/types.ts
+++ src/types.ts
@@ -11,10 +11,25 @@ export interface CanvasImage {
     fontSize?: number
 }
 
+export interface Arrow {
+    id: string
+    x1: number
+    y1: number
+    x2: number
+    y2: number
+    selected?: boolean
+}
+
 export interface FilesContextType {
     uploadedFiles: CanvasImage[]
     setUploadedFiles: React.Dispatch<React.SetStateAction<CanvasImage[]>>
     removeFile: (id: string) => void
+    arrows: Arrow[]
+    setArrows: React.Dispatch<React.SetStateAction<Arrow[]>>
+    addArrow: (arrow: Arrow) => void
+    updateArrow: (id: string, updates: Partial<Arrow>) => void
+    removeArrow: (id: string) => void
+    clearArrows: () => void
 }
 
 export interface FilesProviderProps {
